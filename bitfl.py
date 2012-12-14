2f07153a21b74c3cd1b192b32c394e09c57e7bf8
Matt Parlette <matthew.parlette@gmail.com>
2012-12-14 13:22:55 -0500

    Got the menu to show.

--- bitfl.py
+++ bitfl.py
@@ -1,5 +1,10 @@
+from menu import MainMenu
+
 done = False
+main_menu = MainMenu()
+
 while not done:
   print "Welcome to Billy in the Fat Lane"
+  main_menu.display()
   done = True
   
--- menu.py
+++ menu.py
@@ -3,17 +3,18 @@ class Menu:
   def __init__(self):
     self.title = None
     self.options = dict()
+    self.indent = 2
   
   def display(self):
     """Show the menu to the user and return their selection."""
     if self.title:
       print self.title
-      print '=',len(self.title)
+      print '=' * len(self.title)
     if self.options:
-      for key,value in self.options:
+      for key,value in self.options.iteritems():
         print "%s. %s" % (key,value)
   
-class MainMenu:
+class MainMenu(Menu):
   def __init__(self):
     self.title = "Main Menu"
     self.options = {'n':'New Game','q':'Quit'}
