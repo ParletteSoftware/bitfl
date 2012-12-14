8ef36c16f9234eea71e7e96e7299dbff6c929043
Matt Parlette <matthew.parlette@gmail.com>
2012-12-14 13:15:23 -0500

    Populated class definitions.

--- menu.py
+++ menu.py
@@ -1,12 +1,20 @@
 
 class Menu:
   def __init__(self):
-    pass
+    self.title = None
+    self.options = dict()
   
   def display(self):
-    pass
+    """Show the menu to the user and return their selection."""
+    if self.title:
+      print self.title
+      print '=',len(self.title)
+    if self.options:
+      for key,value in self.options:
+        print "%s. %s" % (key,value)
   
 class MainMenu:
   def __init__(self):
-    pass
+    self.title = "Main Menu"
+    self.options = {'n':'New Game','q':'Quit'}
   
