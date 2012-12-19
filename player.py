8832f5987dbaa95e2828aa0709df1ee13cc6906b
Matt Parlette <matthew.parlette@gmail.com>
2012-12-19 13:29:50 -0500

    Add turns variable

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ class Player:
   def __init__(self,name = "Player"):
     self.name = name
     self.location = None
+    self.turns = 0
   
   def move_player(self,new_location):
     """Change the player's location variable.
