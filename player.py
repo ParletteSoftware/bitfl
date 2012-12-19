71ac3fb26a98d14cba38c8142e789992f91bdcf0
Matt Parlette <matthew.parlette@gmail.com>
2012-12-19 13:27:43 -0500

    Implemented move_player to change the location. This finishes the Player class for now, fixes #3

--- player.py
+++ player.py
@@ -3,4 +3,8 @@ class Player:
     self.name = name
     self.location = None
   
-  
+  def move_player(self,new_location):
+    """Change the player's location variable.
+    The validity of this move should be done before this function is called."""
+    self.location = new_location
+    return True
