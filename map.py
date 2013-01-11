19a300c2666af9c37fcd100f342cf4253587d3fc
Matt Parlette <matthew.parlette@gmail.com>
2013-01-11 14:32:53 -0500

    Start the map class, initialize the game grid

---
+++ map.py
@@ -0,0 +1,7 @@
+from numpy import *
+
+class Map:
+  def __init__(self, x = 10, y = 10):
+    #Create the grid
+    self.grid = empty((x,y),dtype='object') #object may be something more specific later
+  
