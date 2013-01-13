2ed8b815e013cd9b6b0d577e0c58d4dc3a82027f
Matt Parlette <matthew.parlette@gmail.com>
2013-01-13 00:09:28 -0500

    Added uuid for player id

--- player.py
+++ player.py
@@ -1,8 +1,16 @@
+from uuid import uuid4
+
 class Player:
   def __init__(self,name = "Player"):
     self.name = name
+    self._id = uuid4()
     self.location = None
     self.turns = 0
+  
+  @property
+  def id(self):
+    """The string representation of the unique ID should be sufficient for anything using the game object to determine which game this is."""
+    str(self._id)
   
   def move_player(self,new_location):
     """Change the player's location variable.
