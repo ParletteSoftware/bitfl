cb56dad5d6787b68c3f81a70b45d0398a1d6f5ff
Matt Parlette <matthew.parlette@gmail.com>
2013-01-21 16:58:12 -0500

    Added source file copyright preamble to available files.

--- menu.py
+++ menu.py
@@ -1,3 +1,20 @@
+"""Billy in the Fat Lane - A Lame Life Simulation Game
+Copyright (C) 2013 Chris Parlette, Matt Parlette
+
+This file is part of Billy in the Fat Lane.
+
+Billy in the Fat Lane is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Billy in the Fat Lane is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Billy in the Fat Lane.  If not, see http://www.gnu.org/licenses/."""
 
 class Menu:
   def __init__(self):
