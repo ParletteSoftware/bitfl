e1523c4f7817f26b05fa9fda5d90c58e67a7eeac
cparlette <cparlette@gmail.com>
2013-01-25 15:30:01 -0500

    Clear screen after any menu choice
    Originally I planned to clear the screen in the menu.py file, but
    that would clear the screen before the results of your choice could
    show on the screen.  I instead put it after any menu option was
    selected.  Fixes #21.

--- bitfl.py
+++ bitfl.py
@@ -30,8 +30,12 @@ done = False
 main_menu = MainMenu()
 
 while not done:
+  """Clear the screen, use cls if Windows or clear if Linux"""
+  os.system('cls' if os.name=='nt' else 'clear')
   print "Welcome to Billy in the Fat Lane"
   selection = main_menu.display().lower()
+  """Clear the screen, use cls if Windows or clear if Linux"""
+  os.system('cls' if os.name=='nt' else 'clear')
   if selection == 'q':
     done = True
   if selection == 'n':
