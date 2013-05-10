8c225d38faa51c163b905617eeb21a293d8bc4e2
Matt Parlette <matthew.parlette@gmail.com>
2013-05-10 16:42:33 -0400

    Added Time class and added this to the attributes dictionary.

--- player.py
+++ player.py
@@ -31,10 +31,11 @@ class Player:
     self.attributes = {"health": Health(10),
                        "knowledge": Knowledge(),
                        "happiness": Happiness(),
-                       "money": Money()}
+                       "money": Money(),
+                       "time": Time()}
   
   def __repr__(self):
-    return str(self.name)
+    return str(self.value)
   
   def __eq__(self,other):
     return self.id == other.id if hasattr(other,"id") else False
@@ -86,7 +87,7 @@ class Attribute(object):
     self.minor_attributes = None
   
   def __repr__(self):
-    return str(self.get())
+    return str(self.name)
   
   def set(self,value = 0, delta = 0):
     """Change the value of this attribute.
@@ -142,3 +143,9 @@ class Money(Attribute):
   def calculate(self):
     pass
 
+class Time(Attribute):
+  def __init__(self,value=0):
+    super(Time,self).__init__("Time",value)
+  
+  def calculate(self):
+    pass
